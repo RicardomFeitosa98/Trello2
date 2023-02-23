9f2498ca448653e9f29e48834dd2574a10f6777d
Ricardo <ricardomfeitosa98@gmail.com>
2023-02-23 15:28:48 -0300

    feat: now a new board is created on button click, new controller for the board

--- scripts/builders/Boardbuilder.js
+++ scripts/builders/Boardbuilder.js
@@ -1,7 +1,12 @@
 class Boardbuilder{
+constructor() { 
+     
+        var b1= document.createElement("div")
+        b1.className="flexbox"
+        b1.innerHTML="<div >Tarefas:</div>;<div >Lista de compras:</div>;<div >Aniversários:</div><div >Reuniões:</div><div >Rolês:</div><div >Elo no OW:</div>"
+        document.body.appendChild(b1)
+         
+}
 
 
-
-
-    
 }
