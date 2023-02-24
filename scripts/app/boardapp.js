aa886b896d610045b43830ac53003d583cd90a84
Ricardo <ricardomfeitosa98@gmail.com>
2023-02-24 13:43:55 -0300

    feat: now the button adds up to 3 new boards

--- scripts/app/boardapp.js
+++ scripts/app/boardapp.js
@@ -1,3 +1,5 @@
 let controler = new BoardController();
 let b1 = document.getElementById("b1")
+let b2 = document.getElementById("b2")
 b1.addEventListener("click",controler.adiciona.bind(controler))
+b2.addEventListener('click',controler.apagaboard.bind(controler))
--- scripts/controller/BoardController.js
+++ scripts/controller/BoardController.js
@@ -4,13 +4,24 @@ class BoardController {
     }
 
     adiciona() {
-      if(document.getElementsByClassName("flexbox").length < 1){
+      if(document.getElementsByClassName("board").length < 1){
         this.board.build("board1")
+        return
       }
-
+      if(document.getElementsByClassName("board").length ==1){
+        this.board.build("board2")
+        return
+      }
+      if(document.getElementsByClassName("board").length ==2){
+        this.board.build("board3")
+        return
+      }
       
     }
+apagaboard(){
+document.getElementById("board1").remove()
 
+ }
   
 
 
